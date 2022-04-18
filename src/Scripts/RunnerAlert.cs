81cf333c5fa14276548af113b2f2a6d05dfec88d
Mohamed Farag Waziery <mohamedxiv@gmail.com>
2022-04-18 14:12:10 +0200

    src Added

---
+++ src/Scripts/RunnerAlert.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunnerAlert : MonoBehaviour
+{
+    public Text text;
+    //private GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.FindWithTag("MainCanvas").GetComponents<Text>()[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Runner is comming";
+    }
+}
